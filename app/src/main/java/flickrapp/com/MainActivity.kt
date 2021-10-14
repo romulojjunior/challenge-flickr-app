24e2927392dd6d66f9c8bcc19e911a9dadf5d413
romulojjunior <romulojjunior@gmail.com>
2021-10-14 09:53:45 -0300

    Project setup.

---
+++ app/src/main/java/flickrapp/com/MainActivity.kt
@@ -0,0 +1,38 @@
+package flickrapp.com
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import flickrapp.com.ui.theme.FlickrAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FlickrAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FlickrAppTheme {
+        Greeting("Android")
+    }
+}
